cd171bff070155ad453d8ed04d2da77def0a5405
AlbertGhazaryan <solidewebsieten@gmail.com>
2026-06-10 18:37:08 +0300

    init

--- app/routes/apps.favorites.jsx
+++ app/routes/apps.favorites.jsx
@@ -153,7 +153,8 @@ export async function loader({ request }) {
     return new Response(
       JSON.stringify({ 
         success: false, 
-        error: "Failed to fetch favorites" 
+        error: "Failed to fetch favorites", 
+        msg : error, 
       }),
       { 
         status: 500,
